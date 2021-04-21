b3d2d132f6c431dd4cf22562ee504dd8f0562419
Njdos <75140859+Njdos@users.noreply.github.com>
2021-04-21 18:29:14 +0300

    Add files via upload

---
+++ src/main/java/com/example/sweater/repos/MessageRepo.java
@@ -0,0 +1,21 @@
+package com.example.sweater.repos;
+
+import com.example.sweater.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Set;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+    List<Message> findByTitleContainingAndHeadingContainingAndPriceGreaterThanEqualAndPriceLessThanEqual (String title, String heading, long price1, long price2);
+
+    List<Message> findByTitleContainingAndPriceGreaterThanEqualAndPriceLessThanEqual (String title, long price1, long price2);
+
+    List<Message> findByHeadingContainingAndPriceGreaterThanEqualAndPriceLessThanEqual (String heading,long price1, long price2);
+
+    List<Message> findByPriceGreaterThanEqualAndPriceLessThanEqual (long price1, long price2);
+
+    List<Message> findAll();
+
+    Set<Message> findById(long id);
+}
